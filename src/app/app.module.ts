d304c8cc76696e51f848f218f9d560df7ba65a96
mohamedelserafy <elserafy02@gmail.com>
2022-01-16 15:26:35 +0200

    dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChessBoardsComponent } from './components/chess-boards/chess-boards.component';
+
+import { NgxChessBoardModule } from 'ngx-chess-board';
+
+import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'board',
+    component: ChessBoardsComponent,
+  },
+  {
+    path: 'single-board/white',
+    component: BoardComponent,
+  },
+  {
+    path: 'single-board/dark',
+    component: BoardComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ChessBoardsComponent, BoardComponent],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChessBoardModule.forRoot(),
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
