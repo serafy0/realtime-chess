4e1e52d855acb2964bf767810a4d6f4d6494dd1e
mohamedelserafy <elserafy02@gmail.com>
2022-04-06 15:10:17 +0200

    game service

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+} from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import { MoveChange } from 'ngx-chess-board';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameService {
+  private allGames: any;
+
+  games: AngularFireList<any>;
+  constructor(db: AngularFireDatabase) {
+    this.games = db.list('games');
+    this.allGames = db.list('games').snapshotChanges();
+  }
+
+  public createNewGame() {
+    this.games.push({
+      name: 'new',
+      FEN: '',
+    });
+  }
+  public deleteGame(id: any) {
+    this.games.remove(id);
+  }
+
+  public getAllGames() {
+    return this.allGames;
+  }
+}
